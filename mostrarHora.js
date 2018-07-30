1899a2aa5f1be1093bc32ebf807ac99dfae08892
mcquiddy <cristian95mc@gmail.com>
2018-07-29 21:57:53 -0600

    Add files via upload

---
+++ mostrarHora.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">
+var pHora = new Date();
+document.write('<br>Hora: '+pHora.getHours(),'<br>Hora UTC: '+pHora.getUTCHours(),'<br>Minutos: '+pHora.getMinutes(),'<br>Segundos: '+pHora.getSeconds());
+</script>
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      param1:req.body.param1.split("").reverse().join(""),
+      param2:req.body.param2.slice(1,4),
+      param3:req.body.param3.toUpperCase(),
+      param4:req.body.param4.charAt(3),
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})
